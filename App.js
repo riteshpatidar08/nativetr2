c0707d27747f327c3f15714e5f30687e6ef45c9f
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-05 10:02:11 +0530

    updated till search bar

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { View, Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { Searchbar } from 'react-native-paper';
+import { StatusBar } from 'react-native';
+import * as React from 'react';
 function App() {
+     const [searchQuery, setSearchQuery] = React.useState('');
+console.log(StatusBar.currentHeight)
   return (
     <SafeAreaProvider style={{flex:1}}>
-      <View>
-        <Text>hello</Text>
+      <View style={{marginTop:StatusBar.currentHeight, padding:20 ,backgroundColor:'maroon' ,height:150 , justifyContent:'center'}}>
+        <Searchbar
+      placeholder="Search"
+      onChangeText={setSearchQuery}
+      value={searchQuery}
+    />
       </View>
-      <ExpoStatusBar style="auto" translucent={true} />
+      <ExpoStatusBar  translucent={true} backgroundColor='maroon' />
     </SafeAreaProvider>
   );
 }
