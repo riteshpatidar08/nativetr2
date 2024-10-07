6dc733f511b9ba3329ec2a81b2cb5c8bcce153fc
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-07 11:05:43 +0530

    updatedCode till the drawer

--- src/DrawerNavigator.js
+++ src/DrawerNavigator.js
@@ -1,13 +1,14 @@
 import React from 'react'
  import { createDrawerNavigator } from '@react-navigation/drawer'
 import TabNavigator from './TabNavigator'
-import { Settings } from 'react-native' ;;
+import Settings from './screens/Settings'
 function DrawerNavigator() {
     const Drawer = createDrawerNavigator()
   return (
     <Drawer.Navigator>
         <Drawer.Screen name="Tab" component={TabNavigator} />
         <Drawer.Screen name="Settings" component={Settings} />
+
        
     </Drawer.Navigator>
   )
